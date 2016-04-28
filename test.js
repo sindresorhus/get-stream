77c287024e98e457f29320325aff68431ed63d25
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-28 22:31:17 +0700

    meta tweaks

--- test.js
+++ test.js
@@ -2,26 +2,26 @@ import fs from 'fs';
 import test from 'ava';
 import bufferEquals from 'buffer-equals';
 import intoStream from 'into-stream';
-import fn from './';
+import m from './';
 
 test('get stream as a string', async t => {
-	t.is(await fn(fs.createReadStream('fixture')), 'unicorn\n');
-	t.is(await fn(fs.createReadStream('fixture'), {encoding: 'hex'}), '756e69636f726e0a');
+	t.is(await m(fs.createReadStream('fixture')), 'unicorn\n');
+	t.is(await m(fs.createReadStream('fixture'), {encoding: 'hex'}), '756e69636f726e0a');
 });
 
 test('get stream as a buffer', async t => {
 	t.true(
-		bufferEquals(await fn.buffer(fs.createReadStream('fixture')),
+		bufferEquals(await m.buffer(fs.createReadStream('fixture')),
 		new Buffer('unicorn\n'))
 	);
 });
 
 test('get stream as an array', async t => {
 	const fixture = fs.createReadStream('index.js', 'utf8');
-	t.is(typeof (await fn.array(fixture))[0], 'string');
+	t.is(typeof (await m.array(fixture))[0], 'string');
 });
 
 test('get object stream as an array', async t => {
 	const fixture = [{foo: true}, {bar: false}];
-	t.deepEqual(await fn.array(intoStream.obj(fixture)), fixture);
+	t.deepEqual(await m.array(intoStream.obj(fixture)), fixture);
 });
