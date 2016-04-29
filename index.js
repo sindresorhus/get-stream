ba5cb8f9c74e52e7e1d557308b9756e4d9f59a29
James Talmage <james.talmage@jrtechnical.com>
2016-04-29 01:53:45 -0400

    DRY refactor. (#9)

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 'use strict';
 var Promise = require('pinkie-promise');
 
-module.exports = function (stream, opts) {
+function getStream(stream, opts) {
 	if (!stream) {
 		return Promise.reject(new Error('Expected a stream'));
 	}
 
 	opts = opts || {};
+	var encoding = opts.encoding || 'utf8';
+	var buffer = encoding === 'buffer';
 
-	var ret = '';
+	if (buffer || encoding === 'array') {
+		encoding = null;
+	}
+
+	var len = 0;
+	var ret = encoding ? '' : [];
 
 	function onData(chunk) {
-		ret += chunk;
+		if (buffer) {
+			len += chunk.length;
+		}
+
+		if (encoding) {
+			ret += chunk;
+		} else {
+			ret.push(chunk);
+		}
 	}
 
 	var p = new Promise(function (resolve, reject) {
-		stream.setEncoding(opts.encoding || 'utf8');
+		if (encoding) {
+			stream.setEncoding(encoding);
+		}
+
 		stream.on('data', onData);
 		stream.on('error', reject);
 		stream.on('end', resolve);
@@ -28,64 +46,16 @@ module.exports = function (stream, opts) {
 	p.then(clean, clean);
 
 	return p.then(function () {
-		return ret;
+		return buffer ? Buffer.concat(ret, len) : ret;
 	});
-};
+}
+
+module.exports = getStream;
 
 module.exports.buffer = function (stream) {
-	if (!stream) {
-		return Promise.reject(new Error('Expected a stream'));
-	}
-
-	var ret = [];
-	var len = 0;
-
-	function onData(chunk) {
-		ret.push(chunk);
-		len += chunk.length;
-	}
-
-	var p = new Promise(function (resolve, reject) {
-		stream.on('data', onData);
-		stream.on('error', reject);
-		stream.on('end', resolve);
-	});
-
-	var clean = function () {
-		stream.removeListener('data', onData);
-	};
-
-	p.then(clean, clean);
-
-	return p.then(function () {
-		return Buffer.concat(ret, len);
-	});
+	return getStream(stream, {encoding: 'buffer'});
 };
 
 module.exports.array = function (stream) {
-	if (!stream) {
-		return Promise.reject(new Error('Expected a stream'));
-	}
-
-	var ret = [];
-
-	function onData(obj) {
-		ret.push(obj);
-	}
-
-	var p = new Promise(function (resolve, reject) {
-		stream.on('data', onData);
-		stream.on('error', reject);
-		stream.on('end', resolve);
-	});
-
-	var clean = function () {
-		stream.removeListener('data', onData);
-	};
-
-	p.then(clean, clean);
-
-	return p.then(function () {
-		return ret;
-	});
+	return getStream(stream, {encoding: 'array'});
 };
