b4b97424b5fe37093a6575b2cb428eb9bf40f832
James Talmage <james.talmage@jrtechnical.com>
2016-04-30 16:44:32 -0400

    Implement maxBuffer checks. Fixes #8. (#12)

--- index.js
+++ index.js
@@ -1,66 +1,85 @@
 'use strict';
 var PassThrough = require('stream').PassThrough;
 var Promise = require('pinkie-promise');
+var objectAssign = require('object-assign');
 
 function getStream(inputStream, opts) {
 	if (!inputStream) {
 		return Promise.reject(new Error('Expected a stream'));
 	}
 
-	var stream;
-	var encoding = (opts && opts.encoding) || 'utf8';
-	var buffer = encoding === 'buffer';
-	var array = encoding === 'array';
+	opts = objectAssign({maxBuffer: Infinity}, opts);
 
-	if (buffer || array) {
+	var stream;
+	var array = opts.array;
+	var encoding = opts.encoding;
+	var maxBuffer = opts.maxBuffer;
+
+	var buffer = encoding === 'buffer';
+	var objectMode = false;
+
+	if (array) {
+		objectMode = !(encoding || buffer);
+	} else {
+		encoding = encoding || 'utf8';
+	}
+
+	if (buffer) {
 		encoding = null;
 	}
 
 	var len = 0;
-	var ret = encoding ? '' : [];
-
-	function onData(chunk) {
-		if (buffer) {
-			len += chunk.length;
-		}
-
-		if (encoding) {
-			ret += chunk;
-		} else {
-			ret.push(chunk);
-		}
-	}
+	var ret = [];
+	var clean;
 
 	var p = new Promise(function (resolve, reject) {
-		stream = new PassThrough({objectMode: array});
+		stream = new PassThrough({objectMode: objectMode});
 		inputStream.pipe(stream);
 
 		if (encoding) {
 			stream.setEncoding(encoding);
 		}
 
+		var onData = function (chunk) {
+			ret.push(chunk);
+
+			if (objectMode) {
+				len = ret.length;
+			} else {
+				len += chunk.length;
+			}
+
+			if (len > maxBuffer) {
+				reject(new Error('maxBuffer exceeded'));
+			}
+		};
+
 		stream.on('data', onData);
 		stream.on('error', reject);
 		stream.on('end', resolve);
-	});
 
-	var clean = function () {
-		stream.removeListener('data', onData);
-	};
+		clean = function () {
+			stream.removeListener('data', onData);
+		};
+	});
 
 	p.then(clean, clean);
 
 	return p.then(function () {
-		return buffer ? Buffer.concat(ret, len) : ret;
+		if (array) {
+			return ret;
+		}
+
+		return buffer ? Buffer.concat(ret, len) : ret.join('');
 	});
 }
 
 module.exports = getStream;
 
-module.exports.buffer = function (stream) {
-	return getStream(stream, {encoding: 'buffer'});
+module.exports.buffer = function (stream, opts) {
+	return getStream(stream, objectAssign({}, opts, {encoding: 'buffer'}));
 };
 
-module.exports.array = function (stream) {
-	return getStream(stream, {encoding: 'array'});
+module.exports.array = function (stream, opts) {
+	return getStream(stream, objectAssign({}, opts, {array: true}));
 };
--- test.js
+++ test.js
@@ -4,10 +4,24 @@ import bufferEquals from 'buffer-equals';
 import intoStream from 'into-stream';
 import m from './';
 
-test('get stream as a string', async t => {
-	t.is(await m(fs.createReadStream('fixture')), 'unicorn\n');
-	t.is(await m(fs.createReadStream('fixture'), {encoding: 'hex'}), '756e69636f726e0a');
-});
+function makeSetup(intoStream) {
+	function setup(streamDef, opts) {
+		return m(intoStream(streamDef), opts);
+	}
+
+	setup.array = function setup(streamDef, opts) {
+		return m.array(intoStream(streamDef), opts);
+	};
+
+	setup.buffer = function setup(streamDef, opts) {
+		return m.buffer(intoStream(streamDef), opts);
+	};
+
+	return setup;
+}
+
+const setup = makeSetup(intoStream);
+setup.obj = makeSetup(intoStream.obj);
 
 test('get stream as a buffer', async t => {
 	t.true(
@@ -23,8 +37,20 @@ test('get stream as an array', async t => {
 });
 
 test('get object stream as an array', async t => {
-	const fixture = [{foo: true}, {bar: false}];
-	t.deepEqual(await m.array(intoStream.obj(fixture)), fixture);
+	const result = await setup.obj.array([{foo: true}, {bar: false}]);
+	t.deepEqual(result, [{foo: true}, {bar: false}]);
+});
+
+test('get non-object stream as an array of strings', async t => {
+	const result = await setup.array(['foo', 'bar'], {encoding: 'utf8'});
+
+	t.deepEqual(result, ['foo', 'bar']);
+});
+
+test('get non-object stream as an array of Buffers', async t => {
+	const result = await setup.array(['foo', 'bar'], {encoding: 'buffer'});
+
+	t.deepEqual(result, [new Buffer('foo'), new Buffer('bar')]);
 });
 
 test('getStream should not affect additional listeners attached to the stream', async t => {
@@ -34,4 +60,24 @@ test('getStream should not affect additional listeners attached to the stream', 
 	fixture.on('data', chunk => t.true(Buffer.isBuffer(chunk)));
 
 	t.is(await m(fixture), 'foobar');
+});
+
+test('maxBuffer throws when size is exceeded', t => {
+	t.throws(setup(['abcd'], {maxBuffer: 3}));
+	t.notThrows(setup(['abc'], {maxBuffer: 3}));
+
+	t.throws(setup.buffer(['abcd'], {maxBuffer: 3}));
+	t.notThrows(setup.buffer(['abc'], {maxBuffer: 3}));
+});
+
+test('maxBuffer applies to length of arrays when in objectMode', t => {
+	t.throws(m.array(intoStream.obj([{a: 1}, {b: 2}, {c: 3}, {d: 4}]), {maxBuffer: 3}), /maxBuffer exceeded/);
+	t.notThrows(m.array(intoStream.obj([{a: 1}, {b: 2}, {c: 3}]), {maxBuffer: 3}));
+});
+
+test('maxBuffer applies to length of data when not in objectMode', t => {
+	t.throws(setup.array(['ab', 'cd', 'ef'], {encoding: 'utf8', maxBuffer: 5}), /maxBuffer exceeded/);
+	t.notThrows(setup.array(['ab', 'cd', 'ef'], {encoding: 'utf8', maxBuffer: 6}));
+	t.throws(setup.array(['ab', 'cd', 'ef'], {encoding: 'buffer', maxBuffer: 5}), /maxBuffer exceeded/);
+	t.notThrows(setup.array(['ab', 'cd', 'ef'], {encoding: 'buffer', maxBuffer: 6}));
 });
