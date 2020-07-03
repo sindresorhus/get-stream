18ada3dd4b68f8aa41ec2c7c409420073f4c0e58
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-07-03 17:04:04 -0300

    Use promisify
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 const {constants: BufferConstants} = require('buffer');
 const {pipeline} = require('stream');
+const {promisify} = require('util');
 const bufferStream = require('./buffer-stream');
+
+const streamPipelinePromisified = promisify(pipeline);
 
 class MaxBufferError extends Error {
 	constructor() {
@@ -33,14 +36,7 @@ async function getStream(inputStream, options) {
 			reject(error);
 		};
 
-		pipeline(inputStream, stream, error => {
-			if (error) {
-				rejectPromise(error);
-				return;
-			}
-
-			resolve();
-		});
+		streamPipelinePromisified(inputStream, stream).then(resolve, rejectPromise);
 
 		stream.on('data', () => {
 			if (stream.getBufferedLength() > maxBuffer) {
