79ff88e2b276218a5780d86860a3040655b3135e
ehmicky <ehmicky@gmail.com>
2023-08-12 17:35:57 +0100

    Use log helpers

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -6,44 +6,55 @@ import {createNodeStreamBinary, createNodeStreamText, createWebStreamBinary, cre
 
 await createFixture();
 
+const logHeader = header => {
+	console.log(`### ${header}\n`);
+};
+
+const logResult = async (name, createStream, task) => {
+	console.log(`- ${name}(): ${await measureTask(createStream, task)}ms`);
+};
+
 try {
-	console.log(`### Node.js stream (${FIXTURE_HUMAN_SIZE}, binary)\n`);
-	console.log(`- getStream(): ${await measureTask(createNodeStreamBinary, getStream)}ms`);
-	console.log(`- text(): ${await measureTask(createNodeStreamBinary, text)}ms`);
-	console.log(`- getStreamAsBuffer(): ${await measureTask(createNodeStreamBinary, getStreamAsBuffer)}ms`);
-	console.log(`- buffer(): ${await measureTask(createNodeStreamBinary, buffer)}ms`);
-	console.log(`- getStreamAsArrayBuffer(): ${await measureTask(createNodeStreamBinary, getStreamAsArrayBuffer)}ms`);
-	console.log(`- arrayBuffer(): ${await measureTask(createNodeStreamBinary, arrayBuffer)}ms`);
-	console.log(`- getStreamAsArray(): ${await measureTask(createNodeStreamBinary, getStreamAsArray)}ms`);
-	console.log(`- stream.toArray(): ${await measureTask(createNodeStreamBinary, stream => stream.toArray())}ms`);
+	logHeader(`Node.js stream (${FIXTURE_HUMAN_SIZE}, binary)`);
+	await logResult('getStream', createNodeStreamBinary, getStream);
+	await logResult('text', createNodeStreamBinary, text);
+	await logResult('getStreamAsBuffer', createNodeStreamBinary, getStreamAsBuffer);
+	await logResult('buffer', createNodeStreamBinary, buffer);
+	await logResult('getStreamAsArrayBuffer', createNodeStreamBinary, getStreamAsArrayBuffer);
+	await logResult('arrayBuffer', createNodeStreamBinary, arrayBuffer);
+	await logResult('getStreamAsArray', createNodeStreamBinary, getStreamAsArray);
+	await logResult('stream.toArray', createNodeStreamBinary, stream => stream.toArray());
 
-	console.log(`\n### Node.js stream (${FIXTURE_HUMAN_SIZE}, text)\n`);
-	console.log(`- getStream(): ${await measureTask(createNodeStreamText, getStream)}ms`);
-	console.log(`- text(): ${await measureTask(createNodeStreamText, text)}ms`);
-	console.log(`- getStreamAsBuffer(): ${await measureTask(createNodeStreamText, getStreamAsBuffer)}ms`);
-	console.log(`- buffer(): ${await measureTask(createNodeStreamText, buffer)}ms`);
-	console.log(`- getStreamAsArrayBuffer(): ${await measureTask(createNodeStreamText, getStreamAsArrayBuffer)}ms`);
-	console.log(`- arrayBuffer(): ${await measureTask(createNodeStreamText, arrayBuffer)}ms`);
-	console.log(`- getStreamAsArray(): ${await measureTask(createNodeStreamText, getStreamAsArray)}ms`);
-	console.log(`- stream.toArray(): ${await measureTask(createNodeStreamText, stream => stream.toArray())}ms`);
+	console.log('');
+	logHeader(`Node.js stream (${FIXTURE_HUMAN_SIZE}, text)`);
+	await logResult('getStream', createNodeStreamText, getStream);
+	await logResult('text', createNodeStreamText, text);
+	await logResult('getStreamAsBuffer', createNodeStreamText, getStreamAsBuffer);
+	await logResult('buffer', createNodeStreamText, buffer);
+	await logResult('getStreamAsArrayBuffer', createNodeStreamText, getStreamAsArrayBuffer);
+	await logResult('arrayBuffer', createNodeStreamText, arrayBuffer);
+	await logResult('getStreamAsArray', createNodeStreamText, getStreamAsArray);
+	await logResult('stream.toArray', createNodeStreamText, stream => stream.toArray());
 
-	console.log(`\n### Web ReadableStream (${FIXTURE_HUMAN_SIZE}, binary)\n`);
-	console.log(`- getStream(): ${await measureTask(createWebStreamBinary, getStream)}ms`);
-	console.log(`- text(): ${await measureTask(createWebStreamBinary, text)}ms`);
-	console.log(`- getStreamAsBuffer(): ${await measureTask(createWebStreamBinary, getStreamAsBuffer)}ms`);
-	console.log(`- buffer(): ${await measureTask(createWebStreamBinary, buffer)}ms`);
-	console.log(`- getStreamAsArrayBuffer(): ${await measureTask(createWebStreamBinary, getStreamAsArrayBuffer)}ms`);
-	console.log(`- arrayBuffer(): ${await measureTask(createWebStreamBinary, arrayBuffer)}ms`);
-	console.log(`- getStreamAsArray(): ${await measureTask(createWebStreamBinary, getStreamAsArray)}ms`);
+	console.log('');
+	logHeader(`Web ReadableStream (${FIXTURE_HUMAN_SIZE}, binary)`);
+	await logResult('getStream', createWebStreamBinary, getStream);
+	await logResult('text', createWebStreamBinary, text);
+	await logResult('getStreamAsBuffer', createWebStreamBinary, getStreamAsBuffer);
+	await logResult('buffer', createWebStreamBinary, buffer);
+	await logResult('getStreamAsArrayBuffer', createWebStreamBinary, getStreamAsArrayBuffer);
+	await logResult('arrayBuffer', createWebStreamBinary, arrayBuffer);
+	await logResult('getStreamAsArray', createWebStreamBinary, getStreamAsArray);
 
-	console.log(`\n### Web ReadableStream (${FIXTURE_HUMAN_SIZE}, text)\n`);
-	console.log(`- getStream(): ${await measureTask(createWebStreamText, getStream)}ms`);
-	console.log(`- text(): ${await measureTask(createWebStreamText, text)}ms`);
-	console.log(`- getStreamAsBuffer(): ${await measureTask(createWebStreamText, getStreamAsBuffer)}ms`);
-	console.log(`- buffer(): ${await measureTask(createWebStreamText, buffer)}ms`);
-	console.log(`- getStreamAsArrayBuffer(): ${await measureTask(createWebStreamText, getStreamAsArrayBuffer)}ms`);
-	console.log(`- arrayBuffer(): ${await measureTask(createWebStreamText, arrayBuffer)}ms`);
-	console.log(`- getStreamAsArray(): ${await measureTask(createWebStreamText, getStreamAsArray)}ms`);
+	console.log('');
+	logHeader(`Web ReadableStream (${FIXTURE_HUMAN_SIZE}, text)`);
+	await logResult('getStream', createWebStreamText, getStream);
+	await logResult('text', createWebStreamText, text);
+	await logResult('getStreamAsBuffer', createWebStreamText, getStreamAsBuffer);
+	await logResult('buffer', createWebStreamText, buffer);
+	await logResult('getStreamAsArrayBuffer', createWebStreamText, getStreamAsArrayBuffer);
+	await logResult('arrayBuffer', createWebStreamText, arrayBuffer);
+	await logResult('getStreamAsArray', createWebStreamText, getStreamAsArray);
 } finally {
 	await deleteFixture();
 }
